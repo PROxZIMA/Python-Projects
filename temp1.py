6531d09c6fb04af1b9e907a66e53af78ffc19ce5
PROxZIMA <pratikbpingale9075@gmail.com>
2020-04-18 14:25:27 +0530

    Add files via upload

---
+++ temp1.py
@@ -0,0 +1,11 @@
+def insertion(x):
+    for i in range(1, a):
+        return i
+
+
+
+a = int(input('Enter length of a list: '))
+print('Enter the list: ')
+x= [int(input()) for x in range(a)]
+print('Your list is:', x)
+print('Sorted list is:', insertion(x))
